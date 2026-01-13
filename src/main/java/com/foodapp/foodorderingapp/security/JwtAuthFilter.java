ec2d43a21d318eb5c16c26aa802777875e172b21
SURYAS1306 <suryasrinivasan1306@gmail.com>
2026-01-14 01:30:53 +0530

    Update JwtAuthFilter.java

--- src/main/java/com/foodapp/foodorderingapp/security/JwtAuthFilter.java
+++ src/main/java/com/foodapp/foodorderingapp/security/JwtAuthFilter.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-@Component
+//@Component
 @AllArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private JwtService jwtService;
